8c933df04525fbeff033d964274bb9fa42c7b005
Daniel Lancheros <daniellancheros@Daniels-MacBook-Air.local>
2023-07-04 11:32:28 -0400

    ADDED useEffect

--- src/components/BeerList.jsx
+++ src/components/BeerList.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BeerCard from './BeerCard'
 
 export default function BeerList() {
     const [beers, setBeers] = useState()
+    const [selectedBeer, setSelectedBeer] = useState()
 
     const getBeers = () => {
         fetch('https://api.sampleapis.com/beers/ale')
@@ -10,9 +11,24 @@ export default function BeerList() {
             .then(setBeers)
             .catch(alert)
     }
+
+    useEffect( () => { // run once on mount
+        getBeers()
+    }, [])
+
+    useEffect( () => { // triggering a side-effect when selectedBeer (state) changes
+        document.title = selectedBeer || "The Beers"
+    }, [selectedBeer])
+
+    useEffect( () => { // right before component umnounts
+        return () => {
+            alert("Thanks for all the beers!")
+        }
+    }, [])
+
     return (
         <main>
-            <button onClick={getBeers}>Get Beers</button>
+            {selectedBeer && <h2>Selected: {selectedBeer}</h2>}
             <section className='beer-list'>
                 {!beers
                     ? <h2>Loading ...</h2>
@@ -20,9 +36,10 @@ export default function BeerList() {
                         <BeerCard
                         key={beer.id}
                         name={beer.name}
+                        image={beer.image} 
                         avgRating={beer.rating.average}
-                        image={beer.image} />
-                        
+                        setSelectedBeer={setSelectedBeer}
+                        />
                     ))
                 }
             </section>
